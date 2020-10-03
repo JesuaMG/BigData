609f70b4e608519525123104ec5614ead3ae3f31
JesuaMG <jesua.manzano16@tectijuana.edu.mx>
2020-10-02 21:56:21 -0700

    Primera mitad de la practica 1

---
+++ Unit 1/Practicas/Practica1.scala
@@ -0,0 +1,35 @@
+// Assessment 1/Practica 1
+
+//1. Desarrollar un algoritmo en scala que calcule el radio de un circulo
+var radio: Double = 0;
+
+def radioPerimetro (perimetro:Double): Unit = {
+    radio = perimetro / (2 * 3.1416);
+    println(f"El radio de un circulo de perimetro: $perimetro%1.2f es $radio%1.3f");
+}
+
+def radioArea (area:Double): Unit = {
+    radio = math.sqrt(area * 3.1416);
+    println(f"El radio de un circulo de area: $area%1.2f es $radio%1.3f");
+}
+
+//2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+
+//3. Dada la variable bird = "tweet", utiliza interpolacion de string para
+//   imprimir "Estoy ecribiendo un tweet"
+var bird :String = "tweet";
+println(f"Estoy escribiendo un $bird%s");
+
+//4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
+//   secuencia "Luke"
+
+//5. Cual es la diferencia entre value y una variable en scala?
+//  val (vlaue) son constantes lo qeu significa que no podemos modificar el valor de esta,
+//  mintras var (variable) si podemos modificar su valor a lo largo del programa
+val constante : Int = 5;
+var variable : Int = 5;
+
+constante = 6; //error: reassignment to val
+variable = 6; 
+//6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
+
