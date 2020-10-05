38e42a73863197aff4fe6919a1060543a0a848c3
Juan Negrete <john.carlos.116@gmail.com>
2020-10-05 07:26:19 -0700

    Elaboracion de puntos 2,4 y 6.

--- Unit 1/Practicas/Practica1.scala
+++ Unit 1/Practicas/Practica1.scala
@@ -14,6 +14,16 @@ def radioArea (area:Double): Unit = {
 }
 
 //2. Desarrollar un algoritmo en scala que me diga si un numero es primo
+def isPrime(i :Int) : Boolean = {
+     if (i <= 1)
+       false
+     else if (i == 2)
+       true
+     else
+       !(2 to (i-1)).exists(x => i % x == 0)
+   }
+
+(1 to 100).foreach(i => if (isPrime(i)) println("%d es numero primo.".format(i)))
 
 //3. Dada la variable bird = "tweet", utiliza interpolacion de string para
 //   imprimir "Estoy ecribiendo un tweet"
@@ -22,6 +32,8 @@ println(f"Estoy escribiendo un $bird%s");
 
 //4. Dada la variable mensaje = "Hola Luke yo soy tu padre!" utiliza slilce para extraer la
 //   secuencia "Luke"
+val mensaje = ("Hola Luke yo soy tu padre!")
+mensaje.slilce(5,9)
 
 //5. Cual es la diferencia entre value y una variable en scala?
 //  val (vlaue) son constantes lo qeu significa que no podemos modificar el valor de esta,
@@ -31,5 +43,8 @@ var variable : Int = 5;
 
 constante = 6; //error: reassignment to val
 variable = 6; 
+
 //6. Dada la tupla (2,4,5,1,2,3,3.1416,23) regresa el numero 3.1416 
+val tupla1 = (2,4,5,1,2,3,3.1416,23)
+tupla1._7
 
