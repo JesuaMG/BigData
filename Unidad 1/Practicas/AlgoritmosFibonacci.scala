0b37b6cbdc67eda0e53f8e1bc8a5ccf64c5581ce
Juan Negrete <john.carlos.116@gmail.com>
2020-10-16 18:07:36 -0700

    Part 2 of practice Fibonacci was added

--- Unidad 1/Practicas/AlgoritmosFibonacci.scala
+++ Unidad 1/Practicas/AlgoritmosFibonacci.scala
@@ -1,8 +1,23 @@
 //Fibonacci sequence algorithms
-//Algorithm 1
 
-//Algorithm 2
-//Version with explicit formula
+
+// #Algorithm #1
+// #Descending recursive version
+
+def fib1(n:Int):Int={
+    if(n<2)
+    {
+        return n
+    }
+    else
+    {
+        return (fib1(n-1)+fib1(n-2))
+    }
+    
+}
+
+// #Algorithm #2
+// #Version with explicit formula
 
 def fib2(n:Int): Double = {
     if(n < 2){
@@ -15,10 +30,22 @@ def fib2(n:Int): Double = {
     }
 }
 
-//Algorithm 3
+// #Algorithm #3
+// #Iterative version
 
-//Algorithm 4
-//Iterative version 2 variables
+def fib3(n:Int): Int = {
+    var a = 0
+    var b = 1
+    var c = 0
+    for (k <-1 to n){
+        c = b + a
+        a = b 
+        b = c
+    }
+    return a
+}
+// #Algorithm #4
+// #Iterative version 2 variables
 def fib4(n:Int): Int = {
     var a = 0
     var b = 1
@@ -29,4 +56,26 @@ def fib4(n:Int): Int = {
     return a
 }
 
-//Algorithm 5
+
+// #Algorithm #5
+// #Iterative version vector
+
+def fib5(n :Int):Int={
+    if(n<2)
+    {
+        return n
+    }
+    else
+    {
+        var vector = new  Array[Int](n+1)
+        vector(0) = 0
+        vector(1) = 1
+
+        for (k<- Range(2,n+1))
+        {
+            vector(k)=vector(k-1)+vector(k-2)
+        }
+        return vector(n)
+    }
+    
+}
