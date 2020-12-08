431b2b184e5a9e1aac0afa2f94d8fcaa1be8ff97
Juan Negrete <john.carlos.116@gmail.com>
2020-12-08 13:12:31 -0800

    Adding Code Practice 9

--- Unit2/Practices/Practice9/Practice9.scala
+++ Unit2/Practices/Practice9/Practice9.scala
@@ -1,2 +1,31 @@
-// PRACTICE #5
+// PRACTICE #9
+// One-vs-Rest classifier
 
+// Import the libraries
+import org.apache.spark.ml.classification.{LogisticRegression, OneVsRest}
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+
+// Upload data file.
+var inputData = spark.read.format("libsvm").load("/opt/spark/data/mllib/sample_multiclass_classification_data.txt")
+
+// Generate the train / test division.
+val Array(train, test) = inputData.randomSplit(Array(0.8, 0.2))
+
+// Instantiate base classifier
+val classifier = new LogisticRegression().setMaxIter(10).setTol(1E-6).setFitIntercept(true)
+
+// Create an instance of the One Vs Rest classifier.
+val ovr = new OneVsRest().setClassifier(classifier)
+
+// Train the multiclass model.
+val ovrModel = ovr.fit(train)
+
+// Score the model on the test data.
+val predictions = ovrModel.transform(test)
+
+// Get evaluator.
+val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+
+// Calculates the classification error in the test data.
+val accuracy = evaluator.evaluate(predictions)
+println(s"Test Error = ${1 - accuracy}")
