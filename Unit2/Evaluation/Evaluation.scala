83875e1bddcc51219c167af58c8739be540fe9c4
JesuaMG <jesua.manzano16@tectijuana.edu.mx>
2020-12-15 13:33:52 -0600

    Evaluation Practice

--- Unit2/Evaluation/Evaluation.scala
+++ Unit2/Evaluation/Evaluation.scala
@@ -31,17 +31,15 @@ object Evaluation {
     val labelIndexer = new StringIndexer().setInputCol("species").setOutputCol("indexedLabel").fit(df)
     val indexed = labelIndexer.transform(df).drop("species").withColumnRenamed("indexedLabel", "label")
     indexed.describe().show()
-
-    // 7. Build the classification model and explain its architecture.
-    
     val assembler = new VectorAssembler().setInputCols(Array("sepal_length","sepal_width","petal_length","petal_width")).setOutputCol("features")
     val  features = assembler.transform(indexed)
     val labelIndexer2 = new StringIndexer().setInputCol("label").setOutputCol("indexedLabel").fit(indexed)
-    println(s"Found labels: ${labelIndexer2.labels.mkString("[", ", ", "]")}")
     
     features.show
 
-    // Data is divided into training (60%) and testing (30%)
+    // 7. Build the classification model and explain its architecture.  
+
+    // Data is divided into training (60%) and testing (40%)
     val splits = features.randomSplit(Array(0.6, 0.4), seed = 1234L)
     val train = splits(0)
     val test = splits(1)
